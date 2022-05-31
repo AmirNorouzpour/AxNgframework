5e9bbf32b4c6331a50b39acf4f6630ed0aef16b3
Amir Norouzpour <Amirnorozpour@gmail.com>
2022-05-31 16:36:27 +0430

    add new theme

--- theme/theme-builder.js
+++ theme/theme-builder.js
@@ -1,26 +1,29 @@
-const less = require('less');
-const LessPluginCleanCSS = require('less-plugin-clean-css');
-const fs = require('fs');
-const themeVars = require('./theme-vars');
+const less = require("less");
+const LessPluginCleanCSS = require("less-plugin-clean-css");
+const fs = require("fs");
+const themeVars = require("./theme-vars");
 
-const appStyles = '../node_modules/@hdm91/ng-zorro-antd/ng-zorro-antd.less'; // style entry path for the application
+const appStyles = "../node_modules/ng-zorro-antd/ng-zorro-antd.less";
 const themeContent = `@import '${appStyles}';`;
 
 themeVars.forEach((theme, index) => {
-  less.render(themeContent, {
-    javascriptEnabled: true,
-    plugins: [new LessPluginCleanCSS({ advanced: true })],
-    modifyVars: {
-      ...theme
-    }
-  }).then(data => {
-    fs.writeFileSync(
-      // output path for the theme style
-      `../src/assets/themes/style.theme${index + 1}.css`,
-      data.css
-    )
-  }).catch(e => {
-    // log the render error
-    console.error(e);
-  });  
+  less
+    .render(themeContent, {
+      javascriptEnabled: true,
+      plugins: [new LessPluginCleanCSS({ advanced: true })],
+      modifyVars: {
+        ...theme,
+      },
+    })
+    .then((data) => {
+      fs.writeFileSync(
+        // output path for the theme style
+        `../src/assets/themes/style.theme${index + 1}.css`,
+        data.css
+      );
+    })
+    .catch((e) => {
+      // log the render error
+      console.error(e);
+    });
 });
--- theme/theme-vars.js
+++ theme/theme-vars.js
@@ -1,18 +1,17 @@
 module.exports = [
-    {
-        "blue-base": "#214252", 
-    },
-    {
-        "blue-base": "#000000",
-    },
-    {
-        "blue-base": "#0c9f29",
-    },
-    {
-        "blue-base": "#63027e",
-    },
-    {
-        "blue-base": "#1f6f8b",
-    }
+  {
+    "blue-base": "#214252",
+  },
+  {
+    "blue-base": "#000000",
+  },
+  {
+    "blue-base": "#28b498",
+  },
+  {
+    "blue-base": "#63027e",
+  },
+  {
+    "blue-base": "#1f6f8b",
+  },
 ];
-
